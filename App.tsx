5fbce7d81a760fc39693ef77f09c65d903945bb3
Abdumutal <aka.orihero@gmail.com>
2024-05-15 22:38:45 +0500

    refactoring

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
-
 import * as React from 'react';
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-// Screens
-import { BizTvScreen, HomeScreen, ChildTvScreen, BizItemScreen, News2Item, TvShowsScreen, AboutProjectScreen } from './app/screens';
+import {
+  BizTvScreen,
+  HomeScreen,
+  ChildTvScreen,
+  BizItemScreen,
+  News2Item,
+  TvShowsScreen,
+  AboutProjectScreen,
+} from './app/screens';
 
 // App Stack Navigator
 const AppStack = createNativeStackNavigator();
@@ -15,16 +21,11 @@ const App = () => (
   <NavigationContainer>
     <AppStack.Navigator
       initialRouteName="HomeScreen"
-      screenOptions={{ headerShown: false }}>
-      <AppStack.Screen name="HomeScreen" component={HomeScreen} />
-      <AppStack.Screen name="BizTvScreen" component={BizTvScreen} />
-      <AppStack.Screen name='ChildTvScreen' component={ChildTvScreen} />
-      <AppStack.Screen name='BizItemScreen' component={BizItemScreen} />
-      <AppStack.Screen name='News2ItemScreen' component={News2Item} />
-      <AppStack.Screen name='TvShowsScreen' component={TvShowsScreen} />
-      <AppStack.Screen name='AboutProjectScreen' component={AboutProjectScreen} />
-
-
+      screenOptions={{headerShown: false}}>
+      <AppStack.Screen
+        name="AboutProjectScreen"
+        component={AboutProjectScreen}
+      />
     </AppStack.Navigator>
   </NavigationContainer>
 );
